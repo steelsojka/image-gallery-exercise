0b94c29cf829760117141a5f3f312d793cad55cf
Steven Sojka <steelsojka@gmail.com>
2020-09-29 19:47:40 -0500

    more clojurescript

--- clojurescript/image-gallery/src/image_gallery/core.cljs
+++ clojurescript/image-gallery/src/image_gallery/core.cljs
@@ -1,17 +1,65 @@
 (ns image-gallery.core
     (:require
-      [reagent.core :as r]
+      [reagent.core :as r :refer [atom]]
       [clojure.string :as str]
       [reagent.dom :as d]))
 
-(defn make-image [url] {:url url :comments []})
-(defn get-images [] [(make-image "http://google.com")
+;; -------------------------
+;; State
+(defn- make-image [url] {:url url :comments []})
+(defn- get-images [] [(make-image "http://google.com")
                      (make-image "http://blorg.com")])
+
+(defonce state (atom {:is-saving false
+                      :active-image nil
+                      :images (get-images)
+                      :pending-comment ""}))
+
+(defn- on-image-click [index]
+  (swap! state assoc :active-image index))
+
+(defn- on-comment-change [text]
+  (swap! state assoc :pending-comment text))
+
+(defn- on-save-comment-click []
+  (swap! state
+         (fn [v]
+           (let [images (:images v)
+                 image (->> (:active-image v)
+                            (images))
+                 comment (:pending-comment v)]
+             (->> (conj (:comment image) comment)
+                  (assoc image :comments)
+                  (assoc images (:active-image v))
+                  (assoc v :images))))))
 
 ;; -------------------------
 ;; Views
 
-(defn image-selector [state]
+(defn image-details []
+  (let [image (get-in @state
+                      [:images (:active-image @state)])]
+    [:image-details
+     [:div (if (not image)
+             [:h1 "Please select an image"]
+             [:div
+              [:div {:class "image-container"}
+               [:img {:src (:url image)}]]
+              [:div {:class "comments-container"}
+               [:h4 "Comments"]
+               [:div {:class "comment-list"} (doall
+                                               (map #(do [:div %1])
+                                                    (:comments image)))]
+               [:div
+                [:textarea {:disable (:is-saving @state)
+                            :value (:pending-comment @state)
+                            :on-change #(on-comment-change (. %1 -target -value))}]
+                [:div
+                 [:button {:on-click #(on-save-comment-click)} (if (:is-saving @state)
+                                                                 "Saving..."
+                                                                 "Comment")]]]]])]]))
+
+(defn image-selector []
   [:image-selector (doall
                      (map-indexed
                        (fn [i v]
@@ -19,22 +67,17 @@
                                 :class (->> (if (= (:active-image @state) i)
                                              ["image-thumbnail" "active"]
                                              ["image-thumbnail"])
-                                           (str/join " "))}
+                                            (str/join " "))}
                           [:img {:src (:url v)
-                                 :on-click #(reset! state (assoc @state :active-image i))}]])
+                                 :on-click #(on-image-click i)}]])
                        (:images @state)))])
 
-(defn image-gallery-view [state]
+(defn image-gallery-view []
   [:image-gallery.column
-   [:div [image-selector state]]])
+   [:div [image-selector]]])
 
 (defn image-gallery-app []
-  (let [state
-        (r/atom {:is-saving false
-                 :active-image nil
-                 :images (get-images)
-                 :pending-comment "Nope"})]
-    [image-gallery-view state]))
+  [image-gallery-view])
 
 ;; -------------------------
 ;; Initialize app
